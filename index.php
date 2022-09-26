b8e1a96a13be6ede895cc076495704d4737ca3ae
insert github real name here <insert github email here>
2022-09-26 17:32:27 -0300

    Cria, Alterar, deletar usuário

--- index.php
+++ index.php
@@ -20,10 +20,22 @@ require_once("config.php");
 //echo json_encode($search);
 
 // carrega um usuário usando o login e a senha
+//$usuario = new Usuario();
+//$usuario->login("root", "!@#$%");
+//echo $usuario;
+
+// Criando um novo usuário
+//$aluno = new Usuario("aluno", "@lun0");
+//$aluno->setDeslogin("aluno");
+//$aluno->setDessenha("@lun0");
+//$aluno->insert();
+//echo $aluno;
 
 $usuario = new Usuario();
-$usuario->login("root", "!@#$%");
+
+$usuario->loadById(12);
+
+$usuario->update("professor", "!@#$%¨%$#");
 
 echo $usuario;
-
 ?>
